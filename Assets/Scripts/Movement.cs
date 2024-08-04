262e21a277816111b35eefc43826ad523a906ec5
ekou123 <ethankouris321@gmail.com>
2024-08-04 10:08:48 +1000

    Adding already created files to git

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrustStrength = 1f;
+    [SerializeField] float rotationThrust = 1f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+         if (Input.GetKey(KeyCode.Space))
+         {
+            rb.AddRelativeForce(0,thrustStrength,0);
+         }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+         {  
+            ApplyRotation(-rotationThrust);
+         }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // Freezing rotation so we can manually rotate
+        transform.Rotate(0, 0, rotationThrust * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // Unfreezing rotation so physics sys tem can take over
+    }
+}
